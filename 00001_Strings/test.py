44f39cd3cdf8df8b7fba0344c489073e59e7c18d
andres@digitalhouse.com <andres@digitalhouse.com>
2018-11-30 16:46:56 -0500

    Mumuki Export on 2018-11-30 16:46:56 -0500

--- 00001_Strings/test.py
+++ 00001_Strings/test.py
@@ -1,3 +1,3 @@
 class TestFixtures(unittest.TestCase):
    def test_print(self):
-      self.assertEquals(printer, "En Pyhton se denomina 'string' a lo que comunmente llamamos 'texto'")
+      self.assertEquals(/*...content...*/, print("En Pyhton se denomina 'string' a lo que comunmente llamamos 'texto'"))
