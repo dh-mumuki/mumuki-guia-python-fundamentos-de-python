1037115410522dc5c2ae669d0eeea9ebb63faee5
andres@digitalhouse.com <andres@digitalhouse.com>
2018-11-30 17:01:35 -0500

    Mumuki Export on 2018-11-30 17:01:35 -0500

--- 00001_Strings/test.py
+++ 00001_Strings/test.py
@@ -1,5 +1,5 @@
-inout = /*...content...*/
-print_test = print "En Pyhton se denomina 'string' a lo que comunmente llamamos 'texto'" 
+input = /*...content...*/
+print_test = 'print "En Pyhton se denomina "string" a lo que comunmente llamamos "texto"' 
 
 class TestFixtures(unittest.TestCase):
    def test_print(self):
