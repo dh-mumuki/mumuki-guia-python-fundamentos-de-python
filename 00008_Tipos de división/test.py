a9b9d3fef30a29998451fbf43a9020a8fba1a984
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-04 10:53:05 -0500

    Mumuki Export on 2018-12-04 10:53:05 -0500

--- 00008_Tipos de división/test.py
+++ 00008_Tipos de división/test.py
@@ -3,4 +3,4 @@ class TestFixtures(unittest.TestCase):
     self.assertEquals(maples_completos, 2)
       
   def test(self):
-    self.assertEquals(peso_cinco_canicas, 83.52)
+    self.assertEquals(peso_cinco_canicas, 83.57)
