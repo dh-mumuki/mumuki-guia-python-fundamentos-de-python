a0477aa7272947eaac650e9ec718d984e8e753da
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-04 19:23:40 -0500

    Mumuki Export on 2018-12-04 19:23:40 -0500

--- 00004_Variables/test.py
+++ 00004_Variables/test.py
@@ -1,9 +1,12 @@
 class TestFixtures(unittest.TestCase):
   def test(self):
-    self.assertNotEqual(mi_nombre, None)
+    self.assertIsNotNone(mi_nombre)
+    
   def test(self):
     self.assertTrue(type(mi_nombre) == str, "Debe ser un string!")
+  
   def test(self):
-    self.assertNotEqual(mi_edad, None)
+    self.assertIsNotNone(mi_edad)
+  
   def test(self):
     self.assertEqual(type(mi_edad), int,  "Debe ser un integer!")
