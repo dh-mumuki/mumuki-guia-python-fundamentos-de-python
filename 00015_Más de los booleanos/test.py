b9c3804db29478f1bfb27873cbede249319f6312
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 08:38:37 -0500

    Mumuki Export on 2018-12-13 08:38:37 -0500

--- 00015_Más de los booleanos/test.py
+++ 00015_Más de los booleanos/test.py
@@ -22,4 +22,7 @@ class TestFixtures(unittest.TestCase):
     self.assertIs(es_mayor, float or int, 'Recuerda definir la variable un_numero_chico!')
     
   def un_numero_chico_esiste(self):
-    self.assertIs(es_menor, float or int, 'Recuerda definir la variable un_numero_chico!')
+    self.assertTrue(es_mayor == un_numero_grande >= un_numero_chico,'Recuerda definir la variable un_numero_chico!')
+    
+  def un_numero_chico_esiste(self):
+    self.assertTrue(es_menor == un_numero_grande <= un_numero_chico,'Recuerda definir la variable un_numero_chico!')
