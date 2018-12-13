1b6db64d910f4e4b2aa8165e2b5494f0f6193219
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 09:36:49 -0500

    Mumuki Export on 2018-12-13 09:36:49 -0500

--- 00014_Booleanos/test.py
+++ 00014_Booleanos/test.py
@@ -2,8 +2,9 @@ class TestFixtures(unittest.TestCase):
   def test_valor_de_verdad_esiste(self):
     self.assertNotEqual(me_gusta_el_helado, None)
     
-def test_valor_de_verdad_esiste(self):
+  def test_valor_de_verdad_esiste(self):
     self.assertNotEqual(me_gusta_el_helado, 1)    
     
   def test_valor_de_verdad(self):
     self.assertEqual(me_gusta_el_helado, True)
+    
