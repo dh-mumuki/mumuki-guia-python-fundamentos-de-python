d891ef0613e58aab282a969dfbfec1b186d327a7
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 07:28:53 -0500

    Mumuki Export on 2018-12-13 07:28:53 -0500

--- 00007_Operaciones con variables/test.py
+++ 00007_Operaciones con variables/test.py
@@ -1,9 +1,21 @@
 class TestFixtures(unittest.TestCase):
   def test_numeros(self):
-    self.assertEquals(gasto_total, 65)
+    self.assertNotEqual(numero_A, None, 'No existe numero_A!')
       
-  def test(self):
-    self.assertEquals(gasto_promedio, 13)
+  def test_numeros(self):
+    self.assertNotEqual(numero_B, None, 'No existe numero_B!')
 
-  def test(self):
-    self.assertEquals(gasto_total_promocion, 67.5)
+  def test_numeros(self):
+    self.assertNotEqual(numero_C, None, 'No existe numero_C!')
+    
+  def test_numeros(self):
+    self.assertNotEqual(resultado_resta, None, 'No existe resultado_resta!')
+    
+  def test_numeros(self):
+    self.assertNotEqual(resultado_suma, None, 'No existe resultado_suma!')
+    
+  def test_numeros(self):
+    self.assertNotEqual(resultado_multiplicacion, None, 'No existe resultado_multiplicacion!')
+
+  def test_numeros(self):
+    self.assertNotEqual(resultado_division, None, 'No existe resultado_division!')
