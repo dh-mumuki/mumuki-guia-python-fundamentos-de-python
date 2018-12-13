1c2ac79ffcdd53e4284948d7787f09bca9148a41
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 08:24:51 -0500

    Mumuki Export on 2018-12-13 08:24:51 -0500

--- 00014_Booleanos/test.py
+++ 00014_Booleanos/test.py
@@ -3,4 +3,4 @@ class TestFixtures(unittest.TestCase):
     self.assertFalse(me_gusta_el_helado, None, 'Recuerda definir la variable me_gusta_el_helado')
     
   def test_valor_de_verdad(self):
-    self.assertTrue(me_gusta_el_helado, True, "Revisa la variable me_gusta_el_helado, no parece ser igual a True")
+    self.assertTrue(me_gusta_el_helado, "Revisa la variable me_gusta_el_helado, no parece ser igual a True")
