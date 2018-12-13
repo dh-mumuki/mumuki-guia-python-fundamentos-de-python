cdcbc9905627ebad7e92594723775fe36337cc30
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 08:57:00 -0500

    Mumuki Export on 2018-12-13 08:57:00 -0500

--- 00015_Más de los booleanos/test.py
+++ 00015_Más de los booleanos/test.py
@@ -1,3 +1,3 @@
 class TestFixtures(unittest.TestCase):
-  def un_numero_chico_esiste(self):
-    self.assertNotEqual(un_numero_chico, None, 'Recuerda definir la variable un_numero_chico!')
+  def test(self):
+    self.assertNotEqual(un_numero_chico, None)
