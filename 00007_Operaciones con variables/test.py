7813fb058d490b862db48a1522317fef096731c5
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 07:46:22 -0500

    Mumuki Export on 2018-12-13 07:46:22 -0500

--- 00007_Operaciones con variables/test.py
+++ 00007_Operaciones con variables/test.py
@@ -1,39 +1,32 @@
 class TestFixtures(unittest.TestCase):
-  def test_numeros(self):
+  def test_numero_A(self):
     self.assertNotEqual(numero_A, None, 'No existe numero_A!')
       
-  def test_numeros(self):
+  def test_numero_B_existencia(self):
     self.assertNotEqual(numero_B, None, 'No existe numero_B!')
 
-  def test_numeros(self):
-    self.assertNotEqual(numero_C, None, 'No existe numero_C!')
-    
-  def test_numeros(self):
+  def test_resultado_resta_existencia(self):
     self.assertNotEqual(resultado_resta, None, 'No existe resultado_resta!')
     
-  def test_numeros(self):
+  def test_resultado_suma_existencia(self):
     self.assertNotEqual(resultado_suma, None, 'No existe resultado_suma!')
     
-  def test_numeros(self):
+  def test_resultado_multiplicacion_existencia(self):
     self.assertNotEqual(resultado_multiplicacion, None, 'No existe resultado_multiplicacion!')
 
-  def test_numeros(self):
+  def test_resultado_division_existencia(self):
     self.assertNotEqual(resultado_division, None, 'No existe resultado_division!')
 
-  def test_numeros(self):
-    self.assertEqual(resultado_division, numero_A / numero_B, 'Revisa resultado_division!')
-
-  def test_numeros(self):
+  def test_resultado_division(self):
     self.assertAlmostEqual(resultado_division, numero_A / numero_B, 'Revisa resultado_division!')
 
-  def test_numeros(self):
-    self.assertEqual(  def test_numeros(self):
-    self.assertEqual(resultado_division, numero_A * numero_B, 'Revisa resultado_division!')
-, numero_A / numero_B, 'Revisa resultado_multiplicacion!')
+  def test_resultado_multiplicacion(self):
+      self.assertEqual(resultado_division, numero_A * numero_B, 'Revisa resultado_multiplicacion!')
 
-  def test_numeros(self):
+  def test_resultado_suma(self):
     self.assertEqual(resultado_suma, numero_A + numero_B, 'Revisa resultado_suma!')
     
-  def test_numeros(self):
-    self.assertTrue(self.assertEqual(resultado_resta, numero_A - numero_B, 'Revisa resultado_resta!') or self.assertEqual(resultado_resta, numero_A - numero_B, 'Revisa resultado_resta!'))
+#  def test_resultado_resta1(self):
+    
+ #   self.assertTrue(self.assertEqual(resultado_resta, numero_A - numero_B, 'Revisa resultado_resta!') or self.assertEqual(resultado_resta, numero_A - numero_B, 'Revisa resultado_resta!'))
 
