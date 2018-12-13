1a0e3303d72f667657fb7f3448707a88e455a772
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 08:04:00 -0500

    Mumuki Export on 2018-12-13 08:04:00 -0500

--- 00007_Operaciones con variables/test.py
+++ 00007_Operaciones con variables/test.py
@@ -5,6 +5,13 @@ class TestFixtures(unittest.TestCase):
   def test_numero_B_existencia(self):
     self.assertNotEqual(numero_B, None, 'No existe numero_B!')
 
+  def test_numero_A_cero(self):
+    self.assertNotEqual(numero_A, 0, 'numero_A debe ser distinto de 0!')
+      
+  def test_numero_B_cero(self):
+    self.assertNotEqual(numero_B, 0, 'numero_B debe ser distinto de 0!')
+
+
   def test_resultado_resta_existencia(self):
     self.assertNotEqual(resultado_resta, None, 'No existe resultado_resta!')
     
