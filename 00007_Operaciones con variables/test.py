7eb71be10f670ac6bf9937b2f58bb41d48ee7acb
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-13 08:26:19 -0500

    Mumuki Export on 2018-12-13 08:26:19 -0500

--- 00007_Operaciones con variables/test.py
+++ 00007_Operaciones con variables/test.py
@@ -23,9 +23,6 @@ class TestFixtures(unittest.TestCase):
   def test_resultado_division_existencia(self):
     self.assertNotEqual(resultado_division, None, 'No existe resultado_division!')
 
-#  def test_resultado_division(self):
-#    self.assertAlmostEqual(resultado_division, numero_A / numero_B, 'Revisa resultado_division!')
-
   def test_resultado_multiplicacion(self):
       self.assertEqual(resultado_multiplicacion, numero_A * numero_B, 'Revisa resultado_multiplicacion!')
 
@@ -33,6 +30,6 @@ class TestFixtures(unittest.TestCase):
     self.assertEqual(resultado_suma, numero_A + numero_B, 'Revisa resultado_suma!')
     
   def test_resultado_resta1(self):
-   self.assertTrue(((resultado_resta == (numero_A - numero_B)) or (resultado_resta == (numero_B - numero_A))), True)
+   self.assertTrue(((resultado_resta == (numero_A - numero_B)) or (resultado_resta == (numero_B - numero_A))), 'Revisa resultado_resta!')
   def test_resultado_division1(self):
-   self.assertTrue((round(resultado_division) == round(numero_A / numero_B) or (round(resultado_division) == round(numero_B / numero_A)), True))
+   self.assertTrue((round(resultado_division) == round(numero_A / numero_B) or (round(resultado_division) == round(numero_B / numero_A)), 'Revisa resultado_division!'))
