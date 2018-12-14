85cb05003080c3dbf8fecf8913d8754f5d36c524
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 14:17:58 -0500

    Mumuki Export on 2018-12-14 14:17:58 -0500

--- 00014_Booleanos/test.py
+++ 00014_Booleanos/test.py
@@ -3,12 +3,7 @@ class TestFixtures(unittest.TestCase):
     self.assertNotEqual(me_gusta_el_helado, None)
     
   def test_valor_de_verdad_esiste(self):
-    try:
-      self.assertNotEqual(me_gusta_el_helado, 1, "Intenta con un valor distino de 1")    
-    except Exception as e:
-      print e
-      raise Exception
-      
+    self.assertNotEqual(me_gusta_el_helado, 1, "Intenta con un valor distino de 1")    
   def test_valor_de_verdad(self):
     self.assertEqual(me_gusta_el_helado, True)
     
