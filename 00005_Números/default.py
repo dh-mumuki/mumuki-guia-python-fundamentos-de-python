618ec2bf5d45c786b73128fc2fba5efaf597baf1
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 14:04:01 -0500

    Mumuki Export on 2018-12-14 14:04:01 -0500

---
+++ 00005_Números/default.py
@@ -0,0 +1,5 @@
+cinco_float =
+
+cinco_integer =
+
+cinco_string = 
--- 00005_Números/test.py
+++ 00005_Números/test.py
@@ -1,12 +1,15 @@
 class TestFixtures(unittest.TestCase):
-  def test_numeros(self):
+  def test_numeros_float(self):
     self.assertEquals(cinco_float, 5.0)
 
-  def test_numeros(self):
+  def test_numeros_float_tipo(self):
     self.assertTrue(type(cinco_float) == float)
       
-  def test(self):
+  def test_numero_integer_valor(self):
     self.assertEquals(cinco_integer, 5)
+  
+  def test_numeros_integer_tipo(self):
+    self.assertTrue(type(cinco_integer) == integer)
 
-  def test(self):
+  def test_numero_string(self):
     self.assertEquals(cinco_string, 'cinco')
