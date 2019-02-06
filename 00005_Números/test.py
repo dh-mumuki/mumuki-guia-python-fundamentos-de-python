74ebe25459eb51dd4879a0689ff5690faca7def4
Andres Furlan <Andres Furlan>
2019-02-06 20:24:07 +0000

    Mumuki Export on 2019-02-06 20:24:07 +0000

--- 00005_Números/test.py
+++ 00005_Números/test.py
@@ -5,6 +5,5 @@ class TestFixtures(unittest.TestCase):
   def test_numero_integer(self):
     self.assertTrue(cinco_integer == 5 and isinstance(cinco_integer, (int)), 'Este numero %s, no es un entero o no tiene el valor requerido'.format( cinco_integer)
   
-
   def test_numero_string(self):
     self.assertTrue((cinco_string == '5') | (cinco_string.lower() == 'cinco'))
