802ccabdcf58865712ba8b130678122ee9444af4
Andres Furlan <Andres Furlan>
2019-02-06 22:54:46 +0000

    Mumuki Export on 2019-02-06 22:54:46 +0000

--- 00014_Booleanos/test.py
+++ 00014_Booleanos/test.py
@@ -3,6 +3,6 @@ class TestFixtures(unittest.TestCase):
     self.assertTrue('me_gusta_el_helado' in globals(), msg="Acordate de definir la variable")
   
   def test_valor_de_verdad(self):
-    self.assertTrue(isinstance(me_gusta_el_helado, (bool)), 'El valor no es un booleano (verdadero o falso)')
+    self.assertTrue(isinstance(me_gusta_el_helado, (bool)), 'El valor no es un booleano ("True o False")')
     self.assertEqual(me_gusta_el_helado, True, 'El valor de verdad no es correcto')
     
