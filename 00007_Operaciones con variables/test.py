2e4840ca9bf41ff9caa95d0bc6aed8bde889f807
Andres Furlan <Andres Furlan>
2019-02-06 21:19:54 +0000

    Mumuki Export on 2019-02-06 21:19:54 +0000

--- 00007_Operaciones con variables/test.py
+++ 00007_Operaciones con variables/test.py
@@ -1,35 +1,35 @@
 class TestFixtures(unittest.TestCase):
-  def test_numero_A(self):
+  def test_existencia_numero_A(self):
     self.assertTrue(numero_A == None, 'No existe numero_A')
       
-  def test_numero_B_existencia(self):
+  def test_existencia_numero_B(self):
     self.assertTrue(numero_B == None, 'No existe numero_B')
 
-  def test_numero_A_cero(self):
+  def test_valor_numero_A(self):
     self.assertTrue(numero_A == 0, 'numero_A debe ser distinto de 0')
       
-  def test_numero_B_cero(self):
+  def test_valor_numero_B(self):
     self.assertTrue(numero_B == 0, 'numero_B debe ser distinto de 0')
 
-  def test_resultado_resta_existencia(self):
+  def test_existencia_resta(self):
     self.assertTrue(resultado_resta == None, 'No existe resultado_resta')
     
-  def test_resultado_suma_existencia(self):
+  def test_existencia_suma(self):
     self.assertTrue(resultado_suma == None, 'No existe resultado_suma')
     
-  def test_resultado_multiplicacion_existencia(self):
+  def test_existencia_multiplicacion(self):
     self.assertTrue(resultado_multiplicacion, None, 'No existe resultado_multiplicacion')
     
-  def test_resultado_division_existencia(self):
+  def test_existencia_division(self):
     self.assertNotEqual(resultado_division, None, 'No existe resultado_division')
 
-  def test_resultado_multiplicacion(self):
+  def test_valor_multiplicacion(self):
       self.assertEqual(resultado_multiplicacion, numero_A * numero_B, 'Revisa resultado_multiplicacion')
 
-  def test_resultado_suma(self):
+  def test_suma_resultado(self):
     self.assertEqual(resultado_suma, numero_A + numero_B, 'Revisa resultado_suma')
     
-  def test_resultado_resta1(self):
+  def test_resta_resultado(self):
    self.assertTrue(((resultado_resta == (numero_A - numero_B)) or (resultado_resta == (numero_B - numero_A))), 'Revisa resultado_resta')
-  def test_resultado_division1(self):
+  def test_division_resultado(self):
    self.assertTrue((round(resultado_division) == round(numero_A / numero_B) or (round(resultado_division) == round(numero_B / numero_A)), 'Revisa resultado_division'))
