32918d5603fce6e34ddb1fbd3503b98b0d455427
Andres Furlan <Andres Furlan>
2019-02-06 19:57:16 +0000

    Mumuki Export on 2019-02-06 19:57:16 +0000

--- 00004_Variables/test.py
+++ 00004_Variables/test.py
@@ -1,6 +1,9 @@
 class TestStringMethods(unittest.TestCase):
   def test_string(self):
-    self.assertTrue(type(mi_nombre) == str, "Tu nombre debe ser un string")
+    def a():
+      import sys
+      return sys.version
+    self.assertTrue(type(mi_nombre) == str, "Tu nombre debe ser un string version"+ a() )
 
   def test_integer(self):
     self.assertTrue(type(mi_edad) == int,  "Tu edad debe ser un integer")
