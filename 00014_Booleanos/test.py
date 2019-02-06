d06fe5f77889e8f64b898fccff4d552fe304a1e4
Andres Furlan <Andres Furlan>
2019-02-06 22:52:39 +0000

    Mumuki Export on 2019-02-06 22:52:39 +0000

--- 00014_Booleanos/test.py
+++ 00014_Booleanos/test.py
@@ -1,6 +1,6 @@
 class TestFixtures(unittest.TestCase):
   def test_helado_existencia(self):
-    self.assertTrue('me_gusta_el_helado' in globals(), msg="¡Acordate de definir la variable!")
+    self.assertTrue('me_gusta_el_helado' in globals(), msg="Acordate de definir la variable")
   
   def test_valor_de_verdad(self):
     self.assertEqual(me_gusta_el_helado, True, 'El valor de verdad no es correcto')
