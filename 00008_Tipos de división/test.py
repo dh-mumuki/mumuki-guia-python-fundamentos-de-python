cbb83561b48b2162d4feded88a599392389798f8
Andres Furlan <Andres Furlan>
2019-02-06 22:49:45 +0000

    Mumuki Export on 2019-02-06 22:49:45 +0000

--- 00008_Tipos de división/test.py
+++ 00008_Tipos de división/test.py
@@ -1,8 +1,10 @@
 class TestFixtures(unittest.TestCase):
-  def test_maples_precision(self):
+  def test_maples_cantidad(self):
     import sys
     
-    self.assertAlmostEqual(maples_completos, 2, sys.version + "La diferencia es significativa.")
+    self.assertAlmostEqual(maples_completos, 2, sys.version + "Revisa la cantidad de maples.")
       
-  def test_canicas_precision(self):
-    self.assertAlmostEqual(peso_cinco_canicas, 84,'La diferencia es significatica')
+  def test_canicas_peso(self):
+    self.assertAlmostEqual(peso_cinco_canicas, 84,'El peso de las canicas no es correcto')
+    
+  
