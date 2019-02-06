f66106468fd55e2688996a5eea1b4064dbeecc96
Andres Furlan <Andres Furlan>
2019-02-06 23:05:06 +0000

    Mumuki Export on 2019-02-06 23:05:06 +0000

--- 00015_Más de los booleanos/test.py
+++ 00015_Más de los booleanos/test.py
@@ -1,30 +1,29 @@
 class TestFixtures(unittest.TestCase):
   
   def un_numero_chico_existe(self):
-    self.assertTrue('un_numero_chico' in globals(), 'no existe el valor "un_numero_chico"')
+    self.assertTrue('un_numero_chico' in globals(), 'no existe el valor un_numero_chico')
     
-  def un_numero_chico_esiste(self):
-    self.assertIsNone(un_numero_grande, 'no existe el valor "un_numero_grande"')
+  def un_numero_grande_existe(self):
+    self.assertTrue('un_numero_grande' in globals(), 'no existe el valor un_numero_grande')
     
-  def es_mayor_esiste(self):
-    self.assertIsNone(es_mayor)
+  def es_mayor_existe(self):
+    self.assertTrue('es_mayor' in globals(), 'no existe el valor es_mayor')
     
-  def es_menor_esiste(self):
-    self.assertIsNone(es_menor)
-
+  def es_menor_existe(self):
+    self.assertTrue('es_menor' in globals(), 'no existe el valor es_menor')
+  
   def un_numero_chico_tipo(self):
-    self.assertTrue(un_numero_chico == float or un_numero_chico == int)
+    self.assertTrue(un_numero_chico == float or un_numero_chico == int, 'La variable un_numero_chico no es un numero')
     
   def un_numero_grande_tipo(self):
-    self.assertTrue(un_numero_grande == float or un_numero_grande == int)
+    self.assertTrue(un_numero_grande == float or un_numero_grande == int, 'La variable un_numero_grande no es un numero')
 
 
   def test_es_mayor(self):
     
-    self.assertGreater(un_numero_grande, un_numero_chico)
-    self.assertTrue(un_numero_grande > un_numero_chico)
-    self.assertTrue(es_menor)
+    self.assertGreater(un_numero_grande, un_numero_chico, 'un_numero_grande no es mayor a un_numero_chico')
+    
     
   def test_es_menor(self):
-    self.assertLessEqual(un_numero_chico, un_numero_grande)
+    self.assertLessEqual(un_numero_chico, un_numero_grande, 'un_numero_chico no es menor o igual a un_numero_grande')
     self.assertFalse(es_mayor)
