8a14124822cfb08b46918a0fe32db33fb3233ae7
Andres Furlan <Andres Furlan>
2019-02-06 22:45:10 +0000

    Mumuki Export on 2019-02-06 22:45:10 +0000

--- 00008_Tipos de división/default.py
+++ 00008_Tipos de división/default.py
@@ -1,8 +1,13 @@
 #Ejercicio 1
+huevos = 15
+huevos_por_maple = 6 
 
 maples_completos = 
 
-
 #Ejercicio 2
- 
-peso_cinco_canicas = 
+peso_bolsa_canicas = 234.0
+canicas_por_bolsa = 14.0
+
+peso_canica =  
+
+peso_cinco_canicas =  
