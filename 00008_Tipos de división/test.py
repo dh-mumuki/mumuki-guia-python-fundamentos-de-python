b3a66407160725a2db3e048beefb6b5c49421a12
Andres Furlan <Andres Furlan>
2019-02-06 22:40:06 +0000

    Mumuki Export on 2019-02-06 22:40:06 +0000

--- 00008_Tipos de división/test.py
+++ 00008_Tipos de división/test.py
@@ -1,6 +1,6 @@
 class TestFixtures(unittest.TestCase):
   def test_numeros(self):
-    self.assertAlmostEqual(maples_completos, 2)
+    self.assertAlmostEqual(maples_completos, 2, "La diferencia es significativa.")
       
   def test(self):
-    self.assertAlmostEqual(peso_cinco_canicas, 84)
+    self.assertAlmostEqual(peso_cinco_canicas, 84,'La diferencia es significatica')
