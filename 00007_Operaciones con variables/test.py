176222a57a24594ca064601359b06b88dbb2951f
Andres Furlan <Andres Furlan>
2019-02-06 21:32:21 +0000

    Mumuki Export on 2019-02-06 21:32:21 +0000

--- 00007_Operaciones con variables/test.py
+++ 00007_Operaciones con variables/test.py
@@ -12,19 +12,20 @@ class TestFixtures(unittest.TestCase):
     self.assertTrue(numero_B != 0, 'numero_B debe ser distinto de 0')
 
   def test_existencia_resta(self):
-    self.assertTrue(resultado_resta == None, 'No existe resultado_resta')
+    self.assertTrue(resultado_resta != None, 'No existe resultado_resta')
     
   def test_existencia_suma(self):
-    self.assertFalse(resultado_suma == None, 'No existe resultado_suma')
+    self.assertTrue(resultado_suma != None, 'No existe resultado_suma')
     
   def test_existencia_multiplicacion(self):
-    self.assertFalse(resultado_multiplicacion, None, 'No existe resultado_multiplicacion')
+    self.assertFalse(resultado_multiplicacion == None, 'No existe resultado_multiplicacion')
     
   def test_existencia_division(self):
-    self.assertFalse(isinstance(resultado_division, (None)), 'No existe resultado_division')
+    
+    self.assertTrue('resultado_division' in globals(), 'No existe resultado_division')
 
   def test_valor_multiplicacion(self):
-      self.assertEqual(resultado_multiplicacion, numero_A * numero_B, 'Revisa resultado_multiplicacion')
+    self.assertEqual(resultado_multiplicacion, numero_A * numero_B, 'Revisa resultado_multiplicacion')
 
   def test_suma_resultado(self):
     self.assertEqual(resultado_suma, numero_A + numero_B, 'Revisa resultado_suma')
