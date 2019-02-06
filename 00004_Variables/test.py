289db5335781f5c0666a644447942cf4b54ddd24
Andres Furlan <Andres Furlan>
2019-02-06 20:10:43 +0000

    Mumuki Export on 2019-02-06 20:10:43 +0000

--- 00004_Variables/test.py
+++ 00004_Variables/test.py
@@ -1,12 +1,10 @@
 class TestStringMethods(unittest.TestCase):
   def test_string(self):
-    
-    
-    self.assertTrue(isinstance(mi_nombre, (str)), "Tu nombre debe ser un string version" )
+    self.assertTrue(isinstance(mi_nombre, (str)), "Tu nombre debe ser un string" )
 
   def test_integer(self):
     import types
     NumberTypes = (types.IntType, types.LongType, types.FloatType, types.ComplexType)
-    self.assertTrue(isinstance(mi_edad, NumberTypes),  "Tu edad debe ser un integer")
+    self.assertTrue(isinstance(mi_edad, NumberTypes),  "Tu edad debe ser un numero")
 
     
