c11fe4ae4dbcafaacd041fbc8c43f8c8b8276355
Andres Furlan <Andres Furlan>
2019-02-06 20:24:20 +0000

    Mumuki Export on 2019-02-06 20:24:20 +0000

--- 00005_Números/test.py
+++ 00005_Números/test.py
@@ -3,7 +3,7 @@ class TestFixtures(unittest.TestCase):
     self.assertTrue(isinstance(cinco_float, (float)) and cinco_float == 5, 'La coma en los tipo de datos floats, es un punto.')
 
   def test_numero_integer(self):
-    self.assertTrue(cinco_integer == 5 and isinstance(cinco_integer, (int)), 'Este numero %s, no es un entero o no tiene el valor requerido'.format( cinco_integer)
+    self.assertTrue(cinco_integer == 5 and isinstance(cinco_integer, (int)), 'Este numero %s, no es un entero o no tiene el valor requerido'.format( cinco_integer)) 
   
   def test_numero_string(self):
     self.assertTrue((cinco_string == '5') | (cinco_string.lower() == 'cinco'))
