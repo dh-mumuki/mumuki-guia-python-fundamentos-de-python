007470586337bc61047b051f09a093d780a858c8
mrios@digitalhouse.com <mrios@digitalhouse.com>
2019-07-25 11:45:05 +0000

    Mumuki Export on 2019-07-25 11:45:05 +0000

--- 00008_Tipos de división/test.py
+++ 00008_Tipos de división/test.py
@@ -3,4 +3,9 @@ class TestFixtures(unittest.TestCase):
     self.assertAlmostEqual(maples_completos, 2, msg="Revisa la cantidad de maples.")
       
   def test_canicas_peso(self):
-    self.assertAlmostEqual(peso_cinco_canicas, 83.57,msg='El peso de las canicas no es correcto') or self.assertAlmostEqual(peso_cinco_canicas, 83.55,msg='El peso de las canicas no es correcto')
+    if self.assertAlmostEqual(peso_cinco_canicas, 83.57,msg='El peso de las canicas no es correcto'):
+      return True
+    elif self.assertAlmostEqual(peso_cinco_canicas, 83.55,msg='El peso de las canicas no es correcto'):
+      return True
+    else:
+      return False
